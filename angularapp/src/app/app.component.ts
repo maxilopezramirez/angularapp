0e4f9e4bb92bfa4b212aaba387f424f66ec1de9f
maxilopezramirez <maxi.lopez.ramirez@gmail.com>
2018-07-11 22:53:37 -0400

    add map

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angularapp';
+
+	lugares:any = [
+		{active: true, nombre: 'floreria'},
+		{active: false, nombre: 'veterinaria'},
+		{active: true, nombre: 'hospital'}
+	];
+	lat:number = -33.4080968;
+	lng:number = -70.6075464;
+  constructor() {
+  	setTimeout(() => {
+  		this.listo = true;
+  	}, 3000 )
+  }
+
+  hacerAlgo() {
+  	alert('haciendo algo')
+  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+  	AgmCoreModule.forRoot({
+	  apiKey: 'AIzaSyDTASlBvTwIgJBkTBddIl3GmB3msvkkcKc'
+	})
   ],
   providers: [],
   bootstrap: [AppComponent]
